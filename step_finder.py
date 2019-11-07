5bf36a14a3a060a9f50ee3430af696e48b511467
Wouter Liefting <w.e.liefting@tudelft.nl>
2019-11-07 15:29:58 +0100

    Step finder without step selection

--- step_finder.py
+++ step_finder.py
@@ -28,17 +28,6 @@ def stepfinder(data, window = 10, alpha = 0.05):
     def apply_window(i):
         return (data[(i - window) : i], data[i : (i + window)])
     
-    # Function to find groups of strictly increasing numbers within list
-    
-    def groupSequence(input_list): 
-        temp_list = cycle(input_list) 
-      
-        next(temp_list) 
-        groups = groupby(input_list, key = lambda j: j + 1 == next(temp_list)) 
-        for k, v in groups: 
-            if k: 
-                yield tuple(v) + (next((next(groups)[1])), )
-    
     # Check whether data set is large enough to be tested
     
     assert len(data) > 2 * window
@@ -47,22 +36,16 @@ def stepfinder(data, window = 10, alpha = 0.05):
     
     alpha = alpha/len(data)
     steps = list()
-    p_values = list()
     for i in range(window + 1,len(data) - window):
         # determine if there is a significant change in mean position: if there
         # is, the position is saved in steps
         current_p_value = stats.ttest_ind(*apply_window(i))[1]
         if current_p_value < alpha:
             steps.append(i)
-            p_values.append(current_p_value)
             
     # Check whether the step finder is not too sensitive
     
     assert len(steps) < len(data)/window
-   
-    # Selecting steps with lowest p-value per group of
-    
-    ...
-    
-    return groupSequence(steps)
+            
+    return steps
 
