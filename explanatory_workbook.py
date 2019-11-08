0bcbfc3ca6fc56f429ec18fd430ec58ad65d8209
Wouter Liefting <w.e.liefting@tudelft.nl>
2019-11-08 13:13:58 +0100

    Final adjustments figures in workbook

--- explanatory_workbook.py
+++ explanatory_workbook.py
@@ -9,6 +9,7 @@
 # %matplotlib inline
 
 # +
+DONT RUN
 import numpy as np
 
 def trace_generator(N,base,step_size, noise_size,t_step_up,t_step_down):
@@ -95,6 +96,7 @@ def trace_simulator(N,base,step_size,noise_size,number_of_traces,max_number_of_s
 # We can use a little script to generate a set of these traces, like this:
 
 # +
+DONT RUN
 import matplotlib.pyplot as plt
 import numpy as np
 from trace_generator_functions import trace_simulator
@@ -132,6 +134,7 @@ import numpy as np
 x = np.load('x_data.npy')
 N = range(100001)
 fig, (axes1, axes2) = plt.subplots(1,2)
+fig.tight_layout()
 
 for i in range(len(x)):
     axes1.plot(N, x[i,:])
@@ -152,6 +155,7 @@ axes2.plot(range(0,1000),x[i,:1000])
 # The step finder compares for each data point the window left and right of this data point using a t-test. When a significant change in the mean position is found, a step is called. However, multiple steps will be called around a real step. Therefore, we select and retain only the first step of a train of consecutive steps and remove the rest. The step finder then returns a list of unique non-consecutive steps.
 
 # +
+DONT RUN
 from scipy import stats
 from itertools import groupby, cycle 
 
@@ -228,6 +232,7 @@ def stepfinder(data, window = 10, alpha = 0.05):
 # To determine the performance of our step finder, we wrote a function (determine_performance) that takes as input the found t_steps and the actual t_steps and determines the percentage of steps correctly identified, as well as the number of false positives. We permit an error of $\eta$, which is one timepoint by default.
 
 # +
+DONT RUN
 import numpy as np
 def find_nearest(array, value):
     """ finds nearest point in an array to a certain value
@@ -308,62 +313,7 @@ axes2.set_ylabel('Counts')
 
 # ### Challenging the stepfinder: traces with more noise
 
-# Finally, we challenge the stepfinder by feeding it a set of increasingly noisy traces. Since we made all those functions before, this can be executed in only a couple lines of code:
-
-# +
-from trace_generator_functions import trace_simulator
-from step_finder import stepfinder
-from performance_analysis_functions import determine_performance
-import numpy as np
-
-
-
-N = np.linspace(0, 1, 100001)
-base = 10000
-step_size = 10
-number_of_traces = 5
-max_number_of_steps = 1000
-mean_percentage_found = []
-std_percentage_found = []
-mean_false_positives = []
-std_false_positives =[]
-
-for noise_size in np.arange(0, 8, 0.5):
-    x, t_step = trace_simulator(N,base,step_size,noise_size,number_of_traces,max_number_of_steps)
-    percentage_peaks_found = np.zeros(number_of_traces)
-    false_positives = np.zeros(number_of_traces)
-    for j in range(number_of_traces):
-        t_step_found = stepfinder(x[j,:])
-        percentage_peaks_found[j], false_positives[j] = determine_performance(t_step_found,t_step[j], etha  = 1)
-     
-    mean_percentage_found.append(np.mean(percentage_peaks_found))
-    std_percentage_found.append(np.std(percentage_peaks_found))
-    mean_false_positives.append(np.mean(false_positives))
-    std_false_positives.append(np.std(false_positives))
-
-
-# +
-import numpy as np
-import matplotlib.pyplot as plt
-# %matplotlib widget
-
-noise_size= np.arange(0,8,0.5)
-
-mean_percentage_found = np.loadtxt('mean_percentage_found.txt')
-std_percentage_found = np.loadtxt('std_percentage_found.txt')
-
-mean_false_positives = np.loadtxt('mean_false_positives.txt')
-std_false_positives = np.loadtxt('std_false_positives.txt')
-
-fig, (axes1, axes2) = plt.subplots(1,2)
-axes1.set_xlabel('Noise size')
-axes1.set_ylabel('Percentage of peaks found')
-axes1.errorbar(noise_size,mean_percentage_found,std_percentage_found)
-
-
-axes2.set_xlabel('Noise size')
-axes2.set_ylabel('False positives')
-axes2.errorbar(noise_size,mean_false_positives,std_false_positives)
+# Finally, we challenge the stepfinder by feeding it a set of increasingly noisy traces.
 
 # +
 import numpy as np
@@ -395,6 +345,62 @@ axes3.plot(N,x)
 axes3.set_ylim([80,140])
 axes3.set_title("noise size  = 5")
 # -
+# Since we made all those functions before, this can be executed in only a couple lines of code:
+
+# +
+DONT RUN
+from trace_generator_functions import trace_simulator
+from step_finder import stepfinder
+from performance_analysis_functions import determine_performance
+import numpy as np
+
+N = np.linspace(0, 1, 100001)
+base = 10000
+step_size = 10
+number_of_traces = 5
+max_number_of_steps = 1000
+mean_percentage_found = []
+std_percentage_found = []
+mean_false_positives = []
+std_false_positives =[]
+
+for noise_size in np.arange(0, 8, 0.5):
+    x, t_step = trace_simulator(N,base,step_size,noise_size,number_of_traces,max_number_of_steps)
+    percentage_peaks_found = np.zeros(number_of_traces)
+    false_positives = np.zeros(number_of_traces)
+    for j in range(number_of_traces):
+        t_step_found = stepfinder(x[j,:])
+        percentage_peaks_found[j], false_positives[j] = determine_performance(t_step_found,t_step[j], etha  = 1)
+     
+    mean_percentage_found.append(np.mean(percentage_peaks_found))
+    std_percentage_found.append(np.std(percentage_peaks_found))
+    mean_false_positives.append(np.mean(false_positives))
+    std_false_positives.append(np.std(false_positives))
+
+
+# +
+import numpy as np
+import matplotlib.pyplot as plt
+
+noise_size= np.arange(0,8,0.5)
+
+mean_percentage_found = np.loadtxt('mean_percentage_found.txt')
+std_percentage_found = np.loadtxt('std_percentage_found.txt')
+
+mean_false_positives = np.loadtxt('mean_false_positives.txt')
+std_false_positives = np.loadtxt('std_false_positives.txt')
+
+fig, (axes1, axes2) = plt.subplots(1,2)
+axes1.set_xlabel('Noise size')
+axes1.set_ylabel('Percentage of peaks found')
+axes1.errorbar(noise_size,mean_percentage_found,std_percentage_found)
+
+
+axes2.set_xlabel('Noise size')
+axes2.set_ylabel('False positives')
+axes2.errorbar(noise_size,mean_false_positives,std_false_positives)
+# -
+
 
 
 
